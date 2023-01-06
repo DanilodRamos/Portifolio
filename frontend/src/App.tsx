8b66222e69b0f863be9f8986b33faa9a11339e73
Danilo Ramos <danilodramos@yahoo.com.br>
2023-01-06 14:19:14 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá Mundo!</h1>
+    <NotificationButton ></NotificationButton >
+    <NotificationButton ></NotificationButton >
+   
+    </>
   )
 }
 
