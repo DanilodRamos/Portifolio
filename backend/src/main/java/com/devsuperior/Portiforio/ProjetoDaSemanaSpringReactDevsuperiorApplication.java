0d6fb57772e9945b3f264bdbfcefc506c805ec32
Danilo Ramos <danilodramos@yahoo.com.br>
2023-01-06 13:10:48 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/Portiforio/ProjetoDaSemanaSpringReactDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Portiforio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaSemanaSpringReactDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaSemanaSpringReactDevsuperiorApplication.class, args);
+	}
+
+}
