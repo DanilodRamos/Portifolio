d6d2bc15c9574604b63c5e73fbc2f6cd04a6194d
Danilo Ramos <danilodramos@yahoo.com.br>
2023-01-06 19:04:00 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/Portiforio/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/Portiforio/controllers/SaleController.java
@@ -1,24 +1,24 @@
 package com.devsuperior.Portiforio.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.devsuperior.Portiforio.entities.Sale;
 import com.devsuperior.Portiforio.services.SaleService;
 
-@RestController
-@RequestMapping(value = "/sales")
 public class SaleController {
-
+	
 	@Autowired
 	private SaleService service;
 
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
 }
